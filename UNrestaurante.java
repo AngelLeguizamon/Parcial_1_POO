1630034b78bb4af121b2fa18570a2c7904d81b4c
Juan Angel <146735840+AngelLeguizamon@users.noreply.github.com>
2024-12-17 12:48:46 -0500

    Update UNrestaurante.java

---
+++ UNrestaurante.java
@@ -0,0 +1,10 @@
+package unrestaurante;
+public class UNrestaurante {
+
+    public static void main(String[] args) {
+        
+        MenuPrincipal opciones = new MenuPrincipal();        //Se crea un objeto "opciones" de la clase MenuPrincipal, Una vez creado el objeto "opciones"
+        opciones.MostrarMenu();                              //Ejecutamos el metodo "MostrarMenu" que tiene el objeto "opciones"
+        
+    }
+}
